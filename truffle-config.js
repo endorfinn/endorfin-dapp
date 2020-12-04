c4f99f1f8b5849821ee16aaa04d9e7a9c62c906a
JSKeum <JSKeum2512@gmail.com>
2020-12-04 11:40:06 +0900

    fix smart contract deploy setting in develop

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,8 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
+      host: '127.0.0.1',
+      network_id: '*',
       port: 8545
     }
   }
