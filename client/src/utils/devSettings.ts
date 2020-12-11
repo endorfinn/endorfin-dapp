3b1eca252be29b308a7da548fa141ed50748b8e0
JSKeum <JSKeum2512@gmail.com>
2020-12-11 17:02:46 +0900

    modify test accounts

--- client/src/utils/devSettings.ts
+++ client/src/utils/devSettings.ts
@@ -1,7 +1,7 @@
 import $ from "jquery";
 
 export const chainLinkRichAddress = '0x98c63b7b319dfbdf3d811530f2ab9dfe4983af9d';
-export const daiRichAddress = '0x2c1709205C0d97b192cc81027BAF179D403E724E';
+export const daiRichAddress = '0x66c57bF505A85A74609D2C83E94Aabb26d691E1F';
 export const etherRichAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
 const apiKey = '';
@@ -26,8 +26,25 @@ export const getChainLinkInstance = async (web3: any) => new Promise((resolve) =
 
 export const devAccCoinSetup = async (web3: any, daiInstance: any, chainLinkInstance: any, sampleAddresses: any[]) => {
   for (const address of sampleAddresses) {
-    await daiInstance.methods.transfer(address, web3.utils.toWei('10000', 'ether')).send({ from: daiRichAddress });
-    await chainLinkInstance.methods.transfer(address, web3.utils.toWei('10000', 'ether')).send({ from: chainLinkRichAddress });
-    await web3.eth.sendTransaction({to: address, from: etherRichAddress, value: web3.utils.toWei("10000", "ether")})
+    await daiInstance.methods.transfer(address, web3.utils.toWei('1000', 'ether')).send( { from: daiRichAddress });
+    await chainLinkInstance.methods.transfer(address, web3.utils.toWei('1000', 'ether')).send({ from: chainLinkRichAddress });
+    await web3.eth.sendTransaction({to: address, from: etherRichAddress, value: web3.utils.toWei("1000", "ether")})
   }
 }
+
+// let web3 = null;
+
+// try {
+//   const provider = new Web3.providers.HttpProvider(
+//     "http://127.0.0.1:8545"
+//   );
+//   web3 = new Web3(provider);
+//   } catch (error) {
+//     console.error(error);
+// }
+
+// const daiInstance = awaitgetDaiInstance(web3);
+// getChainLinkInstance(web3);
+// devAccCoinSetup(web3, )
+
+
